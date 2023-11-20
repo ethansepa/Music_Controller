faaaf36a8da62731c1e6f1d4e4ace19321c618ad
ethansepa <ethanmsepa@gmail.com>
2023-11-20 14:53:59 -0800

    Dockerized Music Controller web app

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lu0n!afcx!a_r+zc3qh@#k8_w2_@(rxxlz9f3l1(kx+8q#q$o%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
