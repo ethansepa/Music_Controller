d7b458e0d7b3712a954f9d46bfb840a140f934d1
ethansepa <ethanmsepa@gmail.com>
2024-03-24 12:52:19 -0700

    Updated credentials

--- music_controller/spotify/credentials.py
+++ music_controller/spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "dab6b4f49923468ea8abf4d00910488d"
 CLIENT_SECRET = "c8b80375013245d4ae2b566c791933ab"
-REDIRECT_URI = "http://13.58.185.46:8000/spotify/redirect"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
